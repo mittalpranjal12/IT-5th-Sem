811ebae358e091be23efabaca46db4d2362a780f
pranjal mittal <pranjalmittal61117@gmail.com>
2020-09-23 01:02:14 +0530

    Delete InvalidCountryException.java

--- Java LAB/Expt_3/Expt-3B/Expt_3/InvalidCountryException.java
+++
@@ -1,9 +0,0 @@
-package Expt_3;
-
-public class InvalidCountryException extends Exception
-{
-	public InvalidCountryException() {
-		super();
-		System.out.println("User Outside India  cannot be registered");
-	}
-}
